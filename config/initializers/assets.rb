148af6ae0b7807ff570b9fc7c0be9cf1635df053
desaichintan007 <desaichintan007@gmail.com>
2017-02-27 18:58:32 +0530

    map and signup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( meta.js google_maps.js admin.js ie.js product_details.js select_skin.js validate_add_to_cart.js validate_contact.js validate_order.js validate_user.js validate_shopping_cart.js admin.css ie.css )
+Rails.application.config.assets.precompile += %w( meta.js google_maps.js connect_carousal.js admin.js ie.js product_details.js select_skin.js validate_add_to_cart.js validate_contact.js validate_order.js validate_user.js validate_shopping_cart.js admin.css ie.css )
 
 Rails.application.config.assets.precompile += %w( muskoka_roastery_logo.svg )
